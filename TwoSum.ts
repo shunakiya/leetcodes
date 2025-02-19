8bb0f6d03d8aefe6a6ac8f2826212c7dd7fe3043
Shun <143667220+shunakiya@users.noreply.github.com>
2025-02-19 12:28:29 -0800

    edited algo

--- TwoSum.ts
+++ TwoSum.ts
@@ -1,22 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    let firstValue:number = 0;
-    let outputArray:number[] = [];
-    let searching = true;
+    const map: Map<number, number> = new Map();
 
-    while(searching){
-        for(let i = 0; i < nums.length; i++){
-            let counter = 0;
-            let secondValue = nums[i + 1 + counter]
-
-            if(nums[firstValue] + secondValue == target && firstValue!= (i + 1 + counter)){
-                outputArray.push(firstValue, i + 1 + counter);
-
-                return outputArray;
-            } else {
-                counter++;
-            }
-        }
-
-        firstValue++;
+    for(let i = 0; i < nums.length; i++){
+        const complement = target - nums[i];
+        if(map.has(complement)){
+            return[map.get(complement)!, i]
+        } 
+        map.set(nums[i], i)
     }
 };
