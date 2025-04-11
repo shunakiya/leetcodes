f680431e5cb3b5a606f4524e7fb2505fa21b8054
Shun <143667220+shunakiya@users.noreply.github.com>
2025-04-11 01:32:20 -0700

    Create 121-BestTimeToBuyAndSellStock.ts

---
+++ 121-BestTimeToBuyAndSellStock.ts
@@ -0,0 +1,19 @@
+function maxProfit(prices: number[]): number {
+
+    if(prices.length <= 0){
+        return 0;
+    }
+
+    let min = prices[0];
+    let max = 0;
+
+    for(let i = 1; i < prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        } else if(prices[i] - min > max){
+            max = prices[i] - min;
+        }
+    }
+
+    return max;
+};
