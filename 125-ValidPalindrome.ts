0dcf0b35a17bbfa42242415740c0e06aeba37921
Shun <143667220+shunakiya@users.noreply.github.com>
2025-04-16 23:56:46 -0700

    Update 125-ValidPalindrome.ts

--- 125-ValidPalindrome.ts
+++ 125-ValidPalindrome.ts
@@ -1,20 +1,19 @@
 function isPalindrome(s: string): boolean {
     let string = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split("");
 
-    if(string.length === 1){
+    if(string.length === 0 || string.length === 1){
         return true;
     }
 
-    let mid = Math.floor(string.length / 2);
-    let left = Math.floor(string.length / 2) - 1;
-    let right = Math.floor(string.length / 2) + 1;
+    let left = 0;
+    let right = string.length - 1;
 
     for(let i = 0; i < string.length / 2; i++){
         if(string[left] != string[right]){
             return false;
         }
-        left--;
-        right++;
+        left++;
+        right--;
     }
 
     return true;
