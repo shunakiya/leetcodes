9e353733650fed3cd2fed766a8fe6209be1d00d3
Shun <143667220+shunakiya@users.noreply.github.com>
2025-04-28 13:56:15 -0700

    Update 271-EncodeAndDecodingStrings.ts

--- 271-EncodeAndDecodingStrings.ts
+++ 271-EncodeAndDecodingStrings.ts
@@ -1,30 +1,24 @@
-encode(strs) {
-      let res = "";
+function encode(strs: string[]): string {
+    let res = "";
+    for(let i = 0; i < strs.length; i++){
+        res += String(strs[i].length) + "#" + strs[i];
+    }
+    return res;
+}
 
-      for(let i = 0; i < strs.length; i++){
-          res += String(strs[i].length) + "#" + strs[i];
-      }
-
-      return res;
-  }
-
-decode(str) {
-      let res = [];
-      let i = 0;
-
-      while(i < str.length){
-          let j = i
-
-          while(str[j] != "#"){
-              j += 1;
-          }
-          
-          let len = parseInt(str.slice(i, j));
-          res.push(str.slice(j + 1, j + 1 + len))
-
-          i = j + 1 + len;
-      }
-      
-      return res;
-  }
+function decode(str: string): string[] {
+    let res = [];
+    let i = 0;
+    while(i < str.length){
+        let j = i;
+        while(str[j] !== "#"){
+            j += 1;
+        }
+        
+        let len = parseInt(str.slice(i, j));
+        res.push(str.slice(j + 1, j + 1 + len));
+        i = j + 1 + len;
+    }
+    
+    return res;
 }
