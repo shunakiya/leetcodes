f0e3c962b11f273e88523bbfc1d9c0c762fe0a5a
Shun <143667220+shunakiya@users.noreply.github.com>
2025-05-05 10:47:16 -0700

    Update 73-SetMatrixZeros.ts

--- 73-SetMatrixZeros.ts
+++ 73-SetMatrixZeros.ts
@@ -2,30 +2,30 @@
  * Do not return anything, modify matrix in-place instead.
  */
 function setZeroes(matrix: number[][]): void {
-    let zeroCols = [];
-    let zeroRows = [];
+    let zeroCols = new Set();
+    let zeroRows = new Set();
 
     for(let row = 0; row < matrix.length; row++){
         for(let col = 0; col < matrix[row].length; col++){
             if(matrix[row][col] === 0){
-                if(!zeroRows.includes(row)) {
-                    zeroRows.push(row);
-                }
-                if(!zeroCols.includes(col)) {
-                    zeroCols.push(col);
-                }
+                zeroRows.add(row);
+
+                zeroCols.add(col);
             }
         }
     }
     
-    for(let i = 0; i < zeroRows.length; i++) {
-        matrix[zeroRows[i]].fill(0);
+    let zeroRowsArray = Array.from(zeroRows) as number[];
+    let zeroColsArray = Array.from(zeroCols) as number[];
+    
+    for(let i = 0; i < zeroRowsArray.length; i++) {
+        matrix[zeroRowsArray[i]].fill(0);
     }
     
     for(let row = 0; row < matrix.length; row++){
-        for(let i = 0; i < zeroCols.length; i++){
-            let zeroColIndex = zeroCols[i];
-            
+        for(let i = 0; i < zeroColsArray.length; i++){
+            let zeroColIndex = zeroColsArray[i];
+
             matrix[row][zeroColIndex] = 0;
         }
     }
