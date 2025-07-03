afc5ca43ceb404890588d9b17119567a88c020a5
Shun <143667220+shunakiya@users.noreply.github.com>
2025-07-03 11:55:30 -0700

    Update 27-RemoveElement.ts

--- 27-RemoveElement.ts
+++ 27-RemoveElement.ts
@@ -1,9 +1,11 @@
 function removeElement(nums: number[], val: number): number {
+    let k = 0
+
     for(let i = 0; i < nums.length; i++){
-        if(nums[i] === val){
-            nums.splice(i, 1);
-            i--;
+        if(nums[i] !== val){
+            nums[k] = nums[i]; 
+            k++;
         }
     }
-    return nums.length;
+    return k;
 };
